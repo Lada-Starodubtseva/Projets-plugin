8cbfec95618a5b7a389de415e49781b482d20b92
Lada-Starodubtseva <e2038512@cmaisonneuve.qc.ca>
2023-11-08 08:49:38 -0500

    Plugin Projets

---
+++ Projet.js
@@ -0,0 +1,46 @@
+function showText() {
+    var text = document.getElementById("projText");
+    text.classList.toggle("show");
+    }
+function showText2() {
+    var text2 = document.getElementById("projText2");
+    text2.classList.toggle("show");
+    }
+function showText3() {
+    var text3 = document.getElementById("projText3");
+    text3.classList.toggle("show");
+    }
+function showText4() {
+    var text4 = document.getElementById("projText4");
+    text4.classList.toggle("show");
+    }
+function showText5() {
+    var text5 = document.getElementById("projText5");
+    text5.classList.toggle("show");
+    }
+
+
+
+
+// activ -> pour les div a translate a droite
+// activ2 -> pour les div a translate a gauche
+function moveDiv1back() {
+    var proj1 = document.getElementById("projet1");
+    proj1.classList.toggle("activ"); 
+}
+function moveDiv2back() {
+    var proj2 = document.getElementById("projet2");
+    proj2.classList.toggle("activ2"); 
+}
+function moveDiv3back() {
+    var proj3 = document.getElementById("projet3");
+    proj3.classList.toggle("activ"); 
+}
+function moveDiv4back() {
+    var proj4 = document.getElementById("projet4");
+    proj4.classList.toggle("activ2"); 
+}
+function moveDiv5back() {
+    var proj5 = document.getElementById("projet5");
+    proj5.classList.toggle("activ"); 
+}
---
+++ Projet.php
@@ -0,0 +1,152 @@
+<?php
+
+/*
+    Plugin name: Afficher-Projet
+    Author: Lada Starodubtseva
+    Plugin uri:
+    Version: 1.0.0
+    Description: Permet d'afficher les Projet du TIM selon les sessions
+*/
+ 
+ 
+ 
+function Projet_enqueue()
+{
+// filemtime // retourne en milliseconde le temps de la dernière
+// plugin_dir_path // retourne le chemin du répertoire du plugin
+// __FILE__ // le fichier en train de s'exécuter
+// wp_enqueue_style() // Intègre le link:css dans la page
+// wp_enqueue_script() // intègre le script dans la page
+// wp_enqueue_scripts // le hook
+ 
+ 
+$version_css = filemtime(plugin_dir_path( __FILE__ ) . "Projet.css");
+$version_js = filemtime(plugin_dir_path(__FILE__) . "Projet.js");
+ 
+    wp_enqueue_style(   'em_plugin_Projet_css',
+                     plugin_dir_url(__FILE__) . "Projet.css",
+                     array(),
+                     $version_css);
+ 
+    wp_enqueue_script(  'em_plugin_Projet_js',
+                    plugin_dir_url(__FILE__) ."Projet.js",
+                    array(),
+                    $version_js,
+                    true);
+}
+add_action('wp_enqueue_scripts', 'Projet_enqueue');
+ 
+ 
+ 
+function creation_Projet(){
+    $contenu = '
+
+    <section class="site__projets">
+    <title>Pojets des eleves</title>
+    <div id="background-deux"></div>
+       
+    <div class="one">
+                <div class="two">
+                    <div class="three">
+                        <div class="four">
+                            <div class="five">
+
+
+                                <div class="container">
+
+                                    <div class="cont-projet">
+                                        <div id="projet1" class="projet" onclick="showText() , moveDiv1back()" >
+                                    
+                                            <h3 class="title">projet 1</h3>
+                                            <img src="img/_LogoTIM.png" alt="">
+
+                                            <p id="projText" class="texteProjet">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+
+                                        <div id="projet2" class="projet" onclick="showText2() , moveDiv2back()" >
+                                            <h3 class="title">projet 2</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText2" class="texteProjet2">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+
+                                        <div id="projet3"  class="projet" onclick="showText3() ,moveDiv3back()" >
+                                            <h3 class="title">projet 3</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText3" class="texteProjet">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+
+                                        <div id="projet4" class="projet" onclick="showText4() , moveDiv4back()" >
+                                            <h3 class="title">projet 4</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText4" class="texteProjet2">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+                                        <div id="projet5" class="projet" onclick="showText5() , moveDiv5back()" >
+                                            <h3 class="title">projet 5</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText5" class="texteProjet">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>';
+    return $contenu;
+}
+ 
+add_shortcode('em_Projet', 'creation_Projet');
+
+?>
