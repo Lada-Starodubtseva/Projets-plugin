e429628c960cadec6bad9e3b24ba8d882c0f79ea
Lada-Starodubtseva <112096860+Lada-Starodubtseva@users.noreply.github.com>
2023-11-15 11:10:25 -0500

    Correction des bugs d'affichage

--- Projet.js
+++ Projet.js
@@ -24,55 +24,51 @@ function showText5() {
 
 // activ -> pour les div a translate a droite
 // activ2 -> pour les div a translate a gauche
-// activ2 -> pour les div a translate a gauche
-// activ2 -> pour les div a translate a gauche
+
 function moveDiv1back() {
     var proj1 = document.getElementById("projet1");
-    var div = document.getElementById("cont-projet")
-    div.classList.toggle("enableBlur"); 
-    proj1.classList.toggle("activ"); 
+    var div = document.getElementById("cont-projet");
+     
+    proj1.classList.toggle("activ");
     proj1.classList.toggle("activ3"); 
     proj1.classList.toggle("disableBlur"); 
-
+    div.classList.toggle("enableBlur");
 }
 
 function moveDiv2back() {
     var proj2 = document.getElementById("projet2");
     var div = document.getElementById("cont-projet");
-    div.classList.toggle("enableBlur"); 
+    
     proj2.classList.toggle("activ2"); 
     proj2.classList.toggle("activ3"); 
     proj2.classList.toggle("disableBlur"); 
-    
+    div.classList.toggle("enableBlur"); 
 }
 function moveDiv3back() {
     var proj3 = document.getElementById("projet3");
     var div = document.getElementById("cont-projet");
-    div.classList.toggle("enableBlur"); 
+   
     proj3.classList.toggle("activ"); 
     proj3.classList.toggle("activ3"); 
     proj3.classList.toggle("disableBlur"); 
-    
+    div.classList.toggle("enableBlur");
 }
 function moveDiv4back() {
     var proj4 = document.getElementById("projet4");
     var div = document.getElementById("cont-projet");
 
-    div.classList.toggle("enableBlur"); 
-
     proj4.classList.toggle("activ2"); 
     proj4.classList.toggle("activ3"); 
     proj4.classList.toggle("disableBlur"); 
-    
+    div.classList.toggle("enableBlur"); 
+
 }
 function moveDiv5back() {
     var proj5 = document.getElementById("projet5");
     var div = document.getElementById("cont-projet");
 
-    div.classList.toggle("enableBlur"); 
-
     proj5.classList.toggle("activ"); 
     proj5.classList.toggle("activ3"); 
     proj5.classList.toggle("disableBlur"); 
-    
+        div.classList.toggle("enableBlur");
 }
