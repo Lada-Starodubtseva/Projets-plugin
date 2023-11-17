94f2344fbf208f7941c9e1f40eb988768c78f8b8
Lada-Starodubtseva <112096860+Lada-Starodubtseva@users.noreply.github.com>
2023-11-17 10:48:41 -0500

    Addition du format desctop

--- Projet.php
+++ Projet.php
@@ -42,98 +42,97 @@ function creation_Projet(){
     $contenu = '
 
     <section class="site__projets">
-    <title>Pojets des eleves</title>
-    <div id="background-deux"></div>
-       
-
-            
+    <h1 class="titleProjets">PROJETS DES ÉLÈVES</h1>
+                <div class="cont-proj">
 
 
-                                <div class="premier-container-projet">
+                    <div class="premier-container-projet" id="scroller">
 
-                                    <div class="cont-projet  disbaleZindex" id="cont-projet">
-                                        <div id="projet1" class="projet" onclick="showText() , moveDiv1back()" >
-                                    
-                                            <h3 class="title">projet 1</h3>
-                                            <img src="img/_LogoTIM.png" alt="">
+                        <div class="cont-projet  disbaleZindex" id="cont-projet">
+                            <div id="projet1" class="projet" onclick="showText() , moveDiv1back()" >
+                        
 
-                                            <p id="projText" class="texteProjet ">
-                                                Lorem, ipsum dolor sit 
-                                                amet consectetur adipisicing 
-                                                elit. Nobis enim blanditiis 
-                                                est quam, porro, animi repellendus 
-                                                veniam libero, quas officia 
-                                                architecto similique! Ab voluptates 
-                                                laboriosam quibusdam tenetur aut dolor 
-                                                pariatur?
-                                            </p>
-                                        </div>
+                                <h3 class="title">projet 1</h3>
+                                <img src="http://localhost:8080/5w5/wp-content/uploads/2023/10/LogoTIM-1.png" alt="">
 
-                                        <div id="projet2" class="projet" onclick="showText2() , moveDiv2back()" >
-                                            <h3 class="title">projet 2</h3>
-                                            <img src="" alt="">
+                                <p id="projText" class="texteProjet ">
+                                    Lorem, ipsum dolor sit 
+                                    amet consectetur adipisicing 
+                                    elit. Nobis enim blanditiis 
+                                    est quam, porro, animi repellendus 
+                                    veniam libero, quas officia 
+                                    architecto similique! Ab voluptates 
+                                    laboriosam quibusdam tenetur aut dolor 
+                                    pariatur?
+                                </p>
+                            </div>
 
-                                            <p id="projText2" class="texteProjet2 ">
-                                                Lorem, ipsum dolor sit 
-                                                amet consectetur adipisicing 
-                                                elit. Nobis enim blanditiis 
-                                                est quam, porro, animi repellendus 
-                                                veniam libero, quas officia 
-                                                architecto similique! Ab voluptates 
-                                                laboriosam quibusdam tenetur aut dolor 
-                                                pariatur?
-                                            </p>
-                                        </div>
+                            <div id="projet2" class="projet" onclick="showText2() , moveDiv2back()" >
+                                <h3 class="title">projet 2</h3>
+                                <img src="http://localhost:8080/5w5/wp-content/uploads/2023/10/LogoTIM-1.png" alt="">
 
-                                        <div id="projet3"  class="projet" onclick="showText3() ,moveDiv3back()" >
-                                            <h3 class="title">projet 3</h3>
-                                            <img src="" alt="">
+                                <p id="projText2" class="texteProjet2 ">
+                                    Lorem, ipsum dolor sit 
+                                    amet consectetur adipisicing 
+                                    elit. Nobis enim blanditiis 
+                                    est quam, porro, animi repellendus 
+                                    veniam libero, quas officia 
+                                    architecto similique! Ab voluptates 
+                                    laboriosam quibusdam tenetur aut dolor 
+                                    pariatur?
+                                </p>
+                            </div>
 
-                                            <p id="projText3" class="texteProjet3 ">
-                                                Lorem, ipsum dolor sit 
-                                                amet consectetur adipisicing 
-                                                elit. Nobis enim blanditiis 
-                                                est quam, porro, animi repellendus 
-                                                veniam libero, quas officia 
-                                                architecto similique! Ab voluptates 
-                                                laboriosam quibusdam tenetur aut dolor 
-                                                pariatur?
-                                            </p>
-                                        </div>
+                            <div id="projet3"  class="projet" onclick="showText3() ,moveDiv3back()" >
+                                <h3 class="title">projet 3</h3>
+                                <img src="http://localhost:8080/5w5/wp-content/uploads/2023/10/LogoTIM-1.png" alt="">
 
-                                        <div id="projet4" class="projet" onclick="showText4() , moveDiv4back()" >
-                                            <h3 class="title">projet 4</h3>
-                                            <img src="" alt="">
+                                <p id="projText3" class="texteProjet3 ">
+                                    Lorem, ipsum dolor sit 
+                                    amet consectetur adipisicing 
+                                    elit. Nobis enim blanditiis 
+                                    est quam, porro, animi repellendus 
+                                    veniam libero, quas officia 
+                                    architecto similique! Ab voluptates 
+                                    laboriosam quibusdam tenetur aut dolor 
+                                    pariatur?
+                                </p>
+                            </div>
 
-                                            <p id="projText4" class="texteProjet4 ">
-                                                Lorem, ipsum dolor sit 
-                                                amet consectetur adipisicing 
-                                                elit. Nobis enim blanditiis 
-                                                est quam, porro, animi repellendus 
-                                                veniam libero, quas officia 
-                                                architecto similique! Ab voluptates 
-                                                laboriosam quibusdam tenetur aut dolor 
-                                                pariatur?
-                                            </p>
-                                        </div>
-                                        <div id="projet5" class="projet" onclick="showText5() , moveDiv5back()" >
-                                            <h3 class="title">projet 5</h3>
-                                            <img src="" alt="">
+                            <div id="projet4" class="projet" onclick="showText4() , moveDiv4back()" >
+                                <h3 class="title">projet 4</h3>
+                                <img src="http://localhost:8080/5w5/wp-content/uploads/2023/10/LogoTIM-1.png" alt="">
 
-                                            <p id="projText5" class="texteProjet5 ">
-                                                Lorem, ipsum dolor sit 
-                                                amet consectetur adipisicing 
-                                                elit. Nobis enim blanditiis 
-                                                est quam, porro, animi repellendus 
-                                                veniam libero, quas officia 
-                                                architecto similique! Ab voluptates 
-                                                laboriosam quibusdam tenetur aut dolor 
-                                                pariatur?
-                                            </p>
-                                        </div>
-                                    </div>
+                                <p id="projText4" class="texteProjet4 ">
+                                    Lorem, ipsum dolor sit 
+                                    amet consectetur adipisicing 
+                                    elit. Nobis enim blanditiis 
+                                    est quam, porro, animi repellendus 
+                                    veniam libero, quas officia 
+                                    architecto similique! Ab voluptates 
+                                    laboriosam quibusdam tenetur aut dolor 
+                                    pariatur?
+                                </p>
+                            </div>
+                            <div id="projet5" class="projet" onclick="showText5() , moveDiv5back()" >
+                                <h3 class="title">projet 5</h3>
+                                <img src="http://localhost:8080/5w5/wp-content/uploads/2023/10/LogoTIM-1.png" alt="">
 
-                                </div>
+                                <p id="projText5" class="texteProjet5 ">
+                                    Lorem, ipsum dolor sit 
+                                    amet consectetur adipisicing 
+                                    elit. Nobis enim blanditiis 
+                                    est quam, porro, animi repellendus 
+                                    veniam libero, quas officia 
+                                    architecto similique! Ab voluptates 
+                                    laboriosam quibusdam tenetur aut dolor 
+                                    pariatur?
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
 
           
     </section>';
